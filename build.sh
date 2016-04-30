2ec58e4cef9ca1f7ef619ed18690fb36e26396f7
Don <don.j.olmstead@gmail.com>
2016-04-30 09:51:25 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+
+# Build the dart project
+# pub build
+
+# Clone the master branch into a new directory
+git clone https://github.com/donny-dont/hugo-project-homepage.git master
+
+# Install node for processing tools
+NPM_CONFIG_LOGLEVEL=info
+NODE_VERSION=6.0.0
+
+curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz"
+tar -xJf "node-v$NODE_VERSION-linux-x64.tar.xz" -C /usr/local --strip-components=1
+rm "node-v$NODE_VERSION-linux-x64.tar.xz"
+  
